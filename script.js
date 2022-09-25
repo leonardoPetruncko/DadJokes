6245af6a16b902bc699de5237e4369fa35d1e9aa
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-09-24 23:20:34 -0300

    api

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const jokeEL = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+generateJoke()
+
+function generateJoke() {
+    const config =  {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com', config) 
+    .then((res) => res.json())
+    .then((data) => {jokeEL.innerHTML = data.joke})
+}
